03b8b68f62e935e80f1f0cff24d2ff8a46c51e20
filipe <filipe.goncalves29@etec.sp.gov.br>
2023-05-13 17:32:13 -0300

    canvas editado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "blue";
-ctx.fillRect(100, 80,77,80)
+canvas.width = 500;
+canvas.height = 500;
+//arco direito do coração
+ctx.moveTo(100,75);
+ctx.arc(75,75, 25, 0, Math.PI, true);
+ctx.stroke();
+//curva esquerda do coração
+ctx.beginPath();
+ctx.moveTo(50, 75);
+ctx.quadraticCurveTo(50, 100, 100, 150);
+ctx.stroke();
+//arco direito go coração
+ctx.moveTo(150,75);
+ctx.arc(125,75, 25, 0, Math.PI, true);
+ctx.stroke();
+//curva direita do coração
+ctx.beginPath();
+ctx.moveTo(150, 75);
+ctx.quadraticCurveTo(150, 100, 100, 150);
+ctx.stroke();
+//base
+ctx.fillStyle = 'red';
+ctx.fillRect(50, 150, 100, 5);
+
+//imagem
+//cria uma variavel de imagem
+var foto = new Image;
+foto.src = '../img/gato.png';
